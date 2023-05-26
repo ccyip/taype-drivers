3a4752743808c069581618eb43915777a9a70c6a
Qianchuan Ye <yeqianchuan@gmail.com>
2023-05-25 22:42:43 -0400

    Fix division

--- lib/plaintext_OInt0.ml
+++ lib/plaintext_OInt0.ml
@@ -9,7 +9,10 @@ let mux s m n = if Bool.of_int s then m else n
 let add = ( + )
 let sub = ( - )
 let mul = ( * )
-let div = ( / )
+(* Return an arbitrary number if divided by zero. This is necessary because in a
+   mux both branches will be executed, so there is no way to guard a
+   division. *)
+let div m n = if n = 0 then -1 else m / n
 let eq m n = m = n |> Bool.to_int
 let le m n = m <= n |> Bool.to_int
 let band m n = (Bool.of_int m && Bool.of_int n) |> Bool.to_int
